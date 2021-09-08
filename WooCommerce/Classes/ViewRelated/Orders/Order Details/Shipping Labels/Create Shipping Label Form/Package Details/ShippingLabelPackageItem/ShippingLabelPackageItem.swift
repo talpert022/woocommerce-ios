26ed25edd3a145d7ac183ae4178d3878c47cc743
Huong Do <hi.huongdo@gmail.com>
2021-09-08 11:17:17 +0700

    Update localization and constants for ShippingLabelPackageItem

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Package Details/ShippingLabelPackageItem/ShippingLabelPackageItem.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Package Details/ShippingLabelPackageItem/ShippingLabelPackageItem.swift
@@ -25,7 +25,22 @@ struct ShippingLabelPackageItem: View {
     }
 }
 
-private extension ShippingLabelPackageItem {}
+private extension ShippingLabelPackageItem {
+    enum Localization {
+        static let itemsToFulfillHeader = NSLocalizedString("ITEMS TO FULFILL", comment: "Header section items to fulfill in Shipping Label Package Detail")
+        static let packageDetailsHeader = NSLocalizedString("PACKAGE DETAILS", comment: "Header section package details in Shipping Label Package Detail")
+        static let packageSelected = NSLocalizedString("Package Selected",
+                                                       comment: "Title of the row for selecting a package in Shipping Label Package Detail screen")
+        static let totalPackageWeight = NSLocalizedString("Total package weight",
+                                                          comment: "Title of the row for adding the package weight in Shipping Label Package Detail screen")
+        static let footer = NSLocalizedString("Sum of products and package weight",
+                                              comment: "Title of the footer in Shipping Label Package Detail screen")
+    }
+
+    enum Constants {
+        static let dividerPadding: CGFloat = 16
+    }
+}
 
 struct ShippingLabelPackageItem_Previews: PreviewProvider {
     static var previews: some View {
